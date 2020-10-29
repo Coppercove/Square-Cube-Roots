03d01fe57927ccec92ac64b9c5ec9806c1fe0f34
Adam <43795585+Coppercove@users.noreply.github.com>
2020-10-29 16:10:10 -0400

    Add files via upload

---
+++ Roots.c
@@ -0,0 +1,32 @@
+/* Adam Montgomery Lab 4 */
+
+#include <stdio.h>
+#include <math.h>
+ 
+ main()
+{
+    int i,n,number;
+ 	float a,b,result;
+ 	
+    printf("Enter the value: ");
+    scanf("%d",&n);
+    while (n<=0)//loops until positive number is entered
+    	{
+		printf("Enter a Positive Number!\n");
+    	scanf("%d",&n);}
+    	
+    	
+ 
+    printf("No.      Square Root      Cubed Root\n",a,n,b,i);//These will display on the top of each column
+    result = cbrt(n);
+	b=n*n*n;//Start of the calculations
+	for(i=1;i<=n;i++)
+    
+    {
+        printf("%d \t     %.2f \t     %.2f \t \n",i,sqrt((double)i),cbrt((double)i),n);
+    }
+     
+    return 0;
+}
+//This program converts whatever number is enetered and give the proper square and cubed roots of said number.
+
